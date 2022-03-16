b386c238302bddb2a47d5e2fba2a8cf13961fe96
radian628 <70352880+radian628@users.noreply.github.com>
2022-03-16 16:24:30 -0700

    added proper user input

--- viewer/src/main.tsx
+++ viewer/src/main.tsx
@@ -11,6 +11,10 @@ const MainCanvas = () => {
 
 
   let canvasRef = canvasDomNode => { 
+    canvasDomNode.addEventListener("click", e => {
+      canvasDomNode.requestPointerLock();
+    })
+
     let renderState;
     let keysDown: KeyPollSource = {};
     document.addEventListener("keydown", e => {
@@ -20,23 +24,57 @@ const MainCanvas = () => {
       keysDown[e.key.toLowerCase()] = false;
     });
 
+    let mouseMoveX = 0;
+    let mouseMoveY = 0;
+    document.addEventListener("mousemove", e => {
+      mouseMoveX += e.movementX;
+      mouseMoveY += e.movementY;
+    })
+
     window.addEventListener("resize", () => {
-      canvasDomNode.width = window.innerWidth;
-      canvasDomNode.height = window.innerHeight;
+      canvasDomNode.width = window.innerWidth/2;
+      canvasDomNode.height = window.innerHeight/2;
       renderState = raymarch.createRenderState({
-        width:  window.innerWidth,
-        height:  window.innerHeight,
+        width:  window.innerWidth/2,
+        height:  window.innerHeight/2,
         canvas: canvasDomNode,
       });
     });
 
     window.dispatchEvent(new Event("resize"));
 
-    let z = 0;
+    let pos: vec3 = [0, 0, 3.97];
+    let rot: quat = [0,0,0,0];
+    quat.identity(rot);
 
     function loop() {
-      console.log(keysDown);
-      if (keysDown.w) z -= 0.001;
+      //console.log(keysDown);
+      quat.rotateY(rot, rot, mouseMoveX / 300);
+      quat.rotateX(rot, rot, mouseMoveY / 300);
+      let deltaRotation = Math.hypot(mouseMoveX, mouseMoveY);
+      mouseMoveX = 0;
+      mouseMoveY = 0;
+      let movementVec: vec3 = [0, 0, 0];
+      if (keysDown.w) {
+        vec3.add(movementVec, movementVec, [0, 0, 1]);
+      } 
+      if (keysDown.a) {
+        vec3.add(movementVec, movementVec, [-1, 0, 0]);
+      } 
+      if (keysDown.s) {
+        vec3.add(movementVec, movementVec, [0, 0, -1]);
+      } 
+      if (keysDown.d) {
+        vec3.add(movementVec, movementVec, [1, 0, 0]);
+      } 
+      if (keysDown.shift) {
+        vec3.add(movementVec, movementVec, [0, -1, 0]);
+      } 
+      if (keysDown[" "]) {
+        vec3.add(movementVec, movementVec, [0, 1, 0]);
+      } 
+      vec3.transformQuat(movementVec, movementVec, rot);
+      vec3.add(pos, pos, vec3.mul(movementVec, movementVec, [0.01, 0.01, 0.01]));
       let renderTask = raymarch.doRenderTask({
         state: renderState,
         subdivX: 1,
@@ -46,21 +84,17 @@ const MainCanvas = () => {
 
         uniforms: {
           camera: {
-            position: vec3.add([0, 0, 0], [0, 0, 3.97], [z, z, z]),
-            rotation: quat.rotateY(
-              quat.identity([0, 0, 0, 0]),
-              quat.identity([0, 0, 0, 0]),
-              3
-            ),
+            position: pos,
+            rotation: rot
           },
           fovs: [1.5 * window.innerWidth / window.innerHeight, 1.5],
-          primaryRaymarchingSteps: 64,
+          primaryRaymarchingSteps: 32,
           reflections: 3,
           isAdditive: false,
-          blendFactor: 0.95,
+          blendFactor: (vec3.length(movementVec) == 0 && deltaRotation == 0) ? 0.95 : 0.5,
           dof: {
             distance: 0.25,
-            amount: 0.0,
+            amount: 0.01,
           },
           fogDensity: 0.0,
           isRealtimeMode: true,
